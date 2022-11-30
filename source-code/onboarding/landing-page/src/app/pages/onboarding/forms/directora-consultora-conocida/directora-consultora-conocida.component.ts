64c5ca3744a81baf3380129f5f882ebe6e136340
Fernando Montero <fmontero04@gmail.com>
2022-11-30 07:24:41 -0400

    fix: opcion ninguno asignacion directoras

--- source-code/onboarding/landing-page/src/app/pages/onboarding/forms/directora-consultora-conocida/directora-consultora-conocida.component.ts
+++ source-code/onboarding/landing-page/src/app/pages/onboarding/forms/directora-consultora-conocida/directora-consultora-conocida.component.ts
@@ -33,11 +33,32 @@ export class DirectoraConsultoraConocidaComponent implements OnInit {
 
   protected recuperarDatosList(prospectoId: number) {
     this.estructuraComService.requestDirectorasDisponiblesPorConsultoraList(prospectoId)
-      .subscribe({next: (body) => this.directoraList = body});
+      .subscribe({next: this.successDirectorasDisponiblesPorConsultoraList});
 
     this.estructuraComService.requestConsultoraRecomendanteList(prospectoId)
-      .subscribe({next: (body) => this.consultoraList = body});
+      .subscribe({next: this.successConsultoraRecomendanteList});
   }
+
+  protected successDirectorasDisponiblesPorConsultoraList = (body: any[]) => {
+    this.directoraList = [
+      {
+        id: 0,
+        nombre: 'No conozco a nadie'
+      },
+      ...body
+    ];
+  }
+
+  protected successConsultoraRecomendanteList = (body: any[]) => {
+    this.consultoraList = [
+      {
+        id: 0,
+        nombre: 'Nadie me recomendó'
+      },
+      ...body
+    ];
+  }
+
 
   submitForm() {
     this.eventManager.next({event: 'SUBMIT_DIRECTORA_CONSULTORA'});
